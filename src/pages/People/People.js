450211c3ab11613aed7352ec3ffe14edd9128316
Irsyad <irsyad@rightcliq.com.my>
2024-05-01 14:09:48 +0700

    feat: integrate people page

--- src/pages/People/People.js
+++ src/pages/People/People.js
@@ -1,9 +1,18 @@
 /** @format */
 
 import { AddCircleOutline } from "@mui/icons-material";
-import { Box, Button, Typography } from "@mui/material";
+import {
+  Alert,
+  Backdrop,
+  Box,
+  Button,
+  CircularProgress,
+  Typography,
+} from "@mui/material";
 import useMediaQuery from "@mui/material/useMediaQuery";
 import { QueryClient, QueryClientProvider } from "@tanstack/react-query";
+import { useStoreActions, useStoreState } from "easy-peasy";
+import { useEffect, useState } from "react";
 import { Helmet } from "react-helmet-async";
 import BasicTabs from "./CustomTabPanel";
 import HeaderInfo from "./HeaderInfo";
@@ -11,8 +20,14 @@ import HeaderInfo from "./HeaderInfo";
 export default function PeoplePage() {
   const isMobile = useMediaQuery("(max-width:768px)");
   const queryClient = new QueryClient();
+  const people = useStoreState((state) => state.people);
+  const getPeople = useStoreActions((actions) => actions.people.getPeople);
+  const [loading, setLoading] = useState(true);
+  const [person, setPerson] = useState(null);
+  const noData = typeof person === "undefined" || person === null;
 
-  const person = {
+  // TODO: remove this
+  const mockPerson = {
     pilotId: 123456,
     firstName: "Bruce",
     lastName: "Wayne",
@@ -39,6 +54,24 @@ export default function PeoplePage() {
     },
   };
 
+  useEffect(() => {
+    try {
+      getPeople({ accessToken: "test" }).then((result) => {
+        if (result === "") {
+          setLoading(false);
+        } else {
+          setPerson(result);
+          // TODO: remove this
+          setPerson(mockPerson);
+          setLoading(false);
+        }
+      });
+    } catch (ex) {
+      alert("fail");
+      setLoading(false);
+    }
+  }, [people]);
+
   return (
     <>
       <Helmet>
@@ -49,31 +82,49 @@ export default function PeoplePage() {
           width: isMobile ? "100%" : "80%",
         }}
       >
-        <QueryClientProvider client={queryClient}>
-          <HeaderInfo person={person} />
-          <Box
-            sx={{
-              border: "1px solid gray",
-              borderRadius: "16px",
-              padding: "12px",
-            }}
-          >
-            <BasicTabs {...person} />
-            <Box
-              sx={{
-                display: "flex",
-                justifyContent: "space-between",
-                alignItems: "center",
-                paddingX: "12px",
-              }}
-            >
-              <Button sx={{ color: "black", width: "32px", minWidth: "0px" }}>
-                <AddCircleOutline fontSize="large" />
-              </Button>
-              <Typography>Active/Pending License: 5</Typography>
-            </Box>
-          </Box>
-        </QueryClientProvider>
+        {noData ? (
+          <>
+            <Alert severity="info" sx={{ fontSize: "15px", marginTop: "8px" }}>
+              No record was found.
+            </Alert>
+          </>
+        ) : (
+          <>
+            <QueryClientProvider client={queryClient}>
+              <HeaderInfo person={person} />
+              <Box
+                sx={{
+                  border: "1px solid gray",
+                  borderRadius: "16px",
+                  padding: "12px",
+                }}
+              >
+                <BasicTabs {...person} />
+                <Box
+                  sx={{
+                    display: "flex",
+                    justifyContent: "space-between",
+                    alignItems: "center",
+                    paddingX: "12px",
+                  }}
+                >
+                  <Button
+                    sx={{ color: "black", width: "32px", minWidth: "0px" }}
+                  >
+                    <AddCircleOutline fontSize="large" />
+                  </Button>
+                  <Typography>Active/Pending License: 5</Typography>
+                </Box>
+              </Box>
+            </QueryClientProvider>
+          </>
+        )}
+        <Backdrop
+          sx={{ color: "#fff", zIndex: (theme) => theme.zIndex.drawer + 1 }}
+          open={loading}
+        >
+          <CircularProgress color="inherit" />
+        </Backdrop>
       </Box>
     </>
   );
